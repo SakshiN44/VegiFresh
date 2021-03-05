9b973f636944cf73a010058f5129f2829e7d2a7a
SakshiN44 <67193023+SakshiN44@users.noreply.github.com>
2021-03-05 20:00:11 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:finalapp/components/horizontal_listview.dart';
+
 void main() {
   runApp(
     MaterialApp(
